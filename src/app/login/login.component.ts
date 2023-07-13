f731d270fa99b3e5e669eeae8919741ca3e5ee37
ChrisNT23 <105956304+ChrisNT23@users.noreply.github.com>
2023-07-13 16:18:54 -0500

    Cambios

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,12 +20,12 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
   respuestaApi: any;
-
   emisores: any;
+  emisorSeleccionado: any='';  
 
   constructor(private router: Router, private http: HttpClient) { };
 
-  emisorSeleccionado: any;
+  
 
   onEmisorSeleccionado() {
     console.log("emisorSeleccionado: ", this.emisorSeleccionado);
@@ -33,6 +33,7 @@ export class LoginComponent {
   }
 
   ngOnInit(): void {
+    this.emisorSeleccionado = null;
     console.log("apiUrl: ", apiUrl);
     this.http.get(`${apiUrl}/Emisores`).subscribe(response => {
 
